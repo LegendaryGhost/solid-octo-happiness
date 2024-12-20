225634523086298bf27ceca05c20e84dc2069b41
Kevin-rm <kevinramarozatovo@gmail.com>
2024-12-20 14:19:19 +0300

    Update AuthController.java

--- src/main/java/mg/itu/cryptomonnaie/controller/AuthController.java
+++ src/main/java/mg/itu/cryptomonnaie/controller/AuthController.java
@@ -2,17 +2,33 @@ package mg.itu.cryptomonnaie.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
 
-    @GetMapping("/inscription")
-    public String inscription() {
+    @RequestMapping("/inscription")
+    public String formulaireInscription() {
         return "auth/inscription";
     }
 
-    @GetMapping("/connexino")
-    public String connexion() {
+    @PostMapping("/inscription")
+    public String inscription() {
+        // TODO : Utilisation de l'API identity-flow
+
+        return null;
+    }
+
+    @GetMapping("/connexion")
+    public String formulaireConnexion() {
         return "auth/connexion";
+    }
+
+    @PostMapping("/connexion")
+    public String connexion() {
+        // TODO : Utilisation de l'API identity-flow
+
+        return null;
     }
 }
