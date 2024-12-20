446d040a1cc20cd1f353113c9a26a0bfd742b30b
Tiarintsoa <rihantiana000@gmail.com>
2024-12-20 11:21:39 +0300

    Spring Boot initialized

---
+++ src/main/java/mg/itu/solid_octo_happiness/SolidOctoHappinessApplication.java
@@ -0,0 +1,13 @@
+package mg.itu.solid_octo_happiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidOctoHappinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidOctoHappinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/itu/solid_octo_happiness/SolidOctoHappinessApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.itu.solid_octo_happiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidOctoHappinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
