5f77237235775769f322caaa40beba518ed4c5b3
Sarobidy Razafy <sarobidyraza101@gmail.com>
2025-01-10 10:25:15 +0300

    Méthode pour mettre à jour toutes les 10 secondes la courbe

---
+++ src/main/java/mg/itu/cryptomonnaie/service/CoursCryptoService.java
@@ -0,0 +1,46 @@
+package mg.itu.cryptomonnaie.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import mg.itu.cryptomonnaie.entity.CoursCrypto;
+import mg.itu.cryptomonnaie.entity.Cryptomonnaie;
+import mg.itu.cryptomonnaie.repository.CoursCryptoRepository;
+import mg.itu.cryptomonnaie.repository.CryptomonnaieRepository;
+
+@Service
+public class CoursCryptoService {
+    @Autowired
+    private CoursCryptoRepository coursCryptoRepository;
+
+    @Autowired
+    private CryptomonnaieRepository cryptomonnaieRepository;
+    private Random random;
+
+    @Scheduled(fixedRate = 10000)
+    public void generateRandomCours() {
+        List<Cryptomonnaie> cryptomonnaies = cryptomonnaieRepository.findAll();
+
+        for (Cryptomonnaie crypto : cryptomonnaies) {
+            CoursCrypto coursActuel = coursCryptoRepository.findCoursActuel(crypto.getId());
+            Double dernierCours = coursActuel.getCoursActuel();
+            CoursCrypto coursCrypto = new CoursCrypto();
+            coursCrypto.setCryptomonnaie(crypto);
+            coursCrypto.setCoursActuel(generateRandomCoursValue(dernierCours));
+            coursCrypto.setDateCours(LocalDateTime.now());
+
+            coursCryptoRepository.save(coursCrypto);
+            System.out.println("Cours généré pour " + crypto.getDesignation() + ": " + coursCrypto.getCoursActuel());
+        }
+    }
+
+    private Double generateRandomCoursValue(Double dernierCours) {
+        Double facteur = (random.nextDouble() * 0.2) - 0.1;
+        return dernierCours + (dernierCours * facteur);
+    }
+}
