ee4d2fc0d5d4ec1bd731f4c8da70df1e62db847d
Sarobidy Razafy <sarobidyraza101@gmail.com>
2025-01-10 09:56:30 +0300

    Transaction par utilisateur

---
+++ src/main/java/mg/itu/cryptomonnaie/service/HistoriqueFondService.java
@@ -0,0 +1,22 @@
+package mg.itu.cryptomonnaie.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mg.itu.cryptomonnaie.entity.HistoriqueFond;
+import mg.itu.cryptomonnaie.entity.Profil;
+import mg.itu.cryptomonnaie.repository.HistoriqueFondRepository;
+
+@Service
+public class HistoriqueFondService {
+    @Autowired
+    HistoriqueFondRepository historiqueFondRepository;
+
+    public List<HistoriqueFond> transactionProfil(Profil profil) {
+        List<HistoriqueFond> historiqueFonds = historiqueFondRepository.findTransactionsProfil(profil.getId());
+        return historiqueFonds;
+    }
+}
