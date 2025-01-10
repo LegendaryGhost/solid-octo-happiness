1387db2e6728a22a4abe4a46dde022a39827e542
Tiarintsoa <rihantiana000@gmail.com>
2025-01-10 11:25:00 +0300

    Historique fond controller

--- src/main/java/mg/itu/cryptomonnaie/controller/HistoriqueFondController.java
+++ src/main/java/mg/itu/cryptomonnaie/controller/HistoriqueFondController.java
@@ -1,8 +1,13 @@
 package mg.itu.cryptomonnaie.controller;
 
 import lombok.AllArgsConstructor;
+import mg.itu.cryptomonnaie.request.HistoriqueFondRequest;
+import mg.itu.cryptomonnaie.service.HistoriqueFondService;
+import mg.itu.cryptomonnaie.service.TypeTransactionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
@@ -10,9 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HistoriqueFondController {
 
+    private final TypeTransactionService typeTransactionService;
+    private final HistoriqueFondService historiqueFondService;
+
     @GetMapping("/form")
-    public String form() {
+    public String form(Model model) {
+	model.addAttribute("historique", new HistoriqueFondRequest());
+	model.addAttribute("types", typeTransactionService.liste());
+
 	return "pages/transaction/formulaire_depot_retrait";
+    }
+
+    @PostMapping("/ajouter")
+    public String ajouter(HistoriqueFondRequest historique) {
+	historiqueFondService.creerHistoriqueFondTemporaire(historique);
+	return "redirect:/historique-fond/form";
     }
 
 }
