29ce7f2d8a17764a041839bf6a70045f91ee7b37
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-04 12:13:05 +0300

    Update AuthenticationManager.java

--- src/main/java/mg/itu/cryptomonnaie/security/AuthenticationManager.java
+++ src/main/java/mg/itu/cryptomonnaie/security/AuthenticationManager.java
@@ -3,9 +3,10 @@ package mg.itu.cryptomonnaie.security;
 import lombok.RequiredArgsConstructor;
 import mg.itu.cryptomonnaie.entity.Profil;
 import mg.itu.cryptomonnaie.service.ProfilService;
-import mg.itu.cryptomonnaie.utils.Utils;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import static mg.itu.cryptomonnaie.utils.Utils.*;
 
 @RequiredArgsConstructor
 @Component
@@ -14,12 +15,12 @@ public class AuthenticationManager {
     private final ProfilService profilService;
 
     public void authenticate(final String email) {
-        Utils.getCurrentSession().setAttribute(AUTHENTICATED_USER_KEY, profilService.getByEmail(email));
+        getCurrentSession().setAttribute(AUTHENTICATED_USER_KEY, profilService.getByEmail(email));
     }
 
     @Nullable
     public Profil getCurrentUser() {
-        return (Profil) Utils.getCurrentSession().getAttribute(AUTHENTICATED_USER_KEY);
+        return (Profil) getCurrentSession().getAttribute(AUTHENTICATED_USER_KEY);
     }
 
     public boolean isUserConnected() {
