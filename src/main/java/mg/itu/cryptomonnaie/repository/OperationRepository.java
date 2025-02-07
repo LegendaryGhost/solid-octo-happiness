735f1c3563958c727f24751ed59592ebdddd601c
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-07 23:48:56 +0300

    Update OperationRepository.java

--- src/main/java/mg/itu/cryptomonnaie/repository/OperationRepository.java
+++ src/main/java/mg/itu/cryptomonnaie/repository/OperationRepository.java
@@ -4,10 +4,8 @@ import mg.itu.cryptomonnaie.entity.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.Nullable;
 
 public interface OperationRepository extends JpaRepository<Operation, Integer> {
@@ -15,12 +13,4 @@ public interface OperationRepository extends JpaRepository<Operation, Integer> {
     List<Operation> findAllByDateHeureEquals(@Nullable LocalDateTime dateHeure);
 
     List<Operation> findAllByUtilisateurId(Integer idUtilisateur);
-
-    @Query("""
-        SELECT shf.operation
-        FROM StatutHistoriqueFonds shf
-        WHERE shf.operation.id = :id
-        ORDER BY shf.dateHeure DESC
-    """)
-    Optional<Operation> findByIdAndLatestStatut(Integer id);
 }
