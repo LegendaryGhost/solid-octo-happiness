4e270e29a5399f60e9dad8715d7aae03dce2f750
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-08 16:48:45 +0300

    mandeha ny courbe

--- src/main/java/mg/itu/cryptomonnaie/controller/CoursCryptoController.java
+++ src/main/java/mg/itu/cryptomonnaie/controller/CoursCryptoController.java
@@ -1,8 +1,12 @@
 package mg.itu.cryptomonnaie.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 
+import mg.itu.cryptomonnaie.entity.CoursCrypto;
 import mg.itu.cryptomonnaie.entity.Cryptomonnaie;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import mg.itu.cryptomonnaie.service.CoursCryptoService;
 import mg.itu.cryptomonnaie.service.CryptomonnaieService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,17 +25,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class CoursCryptoController {
     private final CoursCryptoService   coursCryptoService;
     private final CryptomonnaieService cryptomonnaieService;
+    private final ObjectMapper objectMapper;
 
     @GetMapping
     public String index(
         Model model,
         @RequestParam(required = false) Integer idCryptomonnaie
-    ) {
+    ) throws JsonProcessingException {
         Cryptomonnaie cryptomonnaie = cryptomonnaieService.getByIdOrGetFirst(idCryptomonnaie);
-        model.addAttribute("coursCryptos", coursCryptoService.getByCryptomonnaie(cryptomonnaie.getId()))
-            .addAttribute("cryptomonnaies", cryptomonnaieService.getAll())
+        model.addAttribute("coursCryptos", objectMapper.writeValueAsString(
+            coursCryptoService.getByCryptomonnaie(cryptomonnaie.getId())
+            )).addAttribute("cryptomonnaies", cryptomonnaieService.getAll())
             .addAttribute("cryptoActuelle", cryptomonnaie.getDesignation());
 
         return "cours";
+    }
+
+    @GetMapping(path = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<CoursCrypto> getCoursData(@RequestParam(required = false) Integer idCryptomonnaie) {
+        return coursCryptoService.getByCryptomonnaie(cryptomonnaieService.getByIdOrGetFirst(idCryptomonnaie).getId());
     }
 }
