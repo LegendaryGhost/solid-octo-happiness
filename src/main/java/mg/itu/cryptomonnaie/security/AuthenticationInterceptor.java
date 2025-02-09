95171f3dd9b57ec0303fbbe07b8b4a420ebdad7d
Sarobidy Razafy <sarobidyraza101@gmail.com>
2025-02-09 19:07:12 +0300

    Page alea 2-3

--- src/main/java/mg/itu/cryptomonnaie/security/AuthenticationInterceptor.java
+++ src/main/java/mg/itu/cryptomonnaie/security/AuthenticationInterceptor.java
@@ -18,20 +18,21 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(
-        @NonNull HttpServletRequest request,
-        @NonNull HttpServletResponse response,
-        @NonNull Object handler
-    ) throws Exception {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler) throws Exception {
         // Uncomment the code below when pushing to production
-        /* if (authenticationManager.isUserConnected()) return true;
-
-        response.sendRedirect("/connexion");
-        return false; */
+        /*
+         * if (authenticationManager.isUserConnected()) return true;
+         * 
+         * response.sendRedirect("/connexion");
+         * return false;
+         */
 
         // Warning !! : This code is for testing purposes only
         if (!authenticationManager.isUserConnected())
             authenticationManager.authenticate(
-                utilisateurService.updateOrCreate("example@domain.com", "unsecure_token_for_testing_purposes"));
+                    utilisateurService.updateOrCreate("example@domain.com", "unsecure_token_for_testing_purposes"));
         return true;
     }
 }
